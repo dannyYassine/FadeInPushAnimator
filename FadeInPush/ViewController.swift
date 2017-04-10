b676633a92994cab84de89967f13a5db59bc0e94
Danny Yassine <dannyyassine@gmail.com>
2017-04-09 20:41:03 -0400

    - added extra views

--- FadeInPush/ViewController.swift
+++ FadeInPush/ViewController.swift
@@ -12,14 +12,19 @@ class ViewController: BaseViewController {
 
     var color: UIColor {
         
-        let colors = [
-            UIColor.red,
-            UIColor.green,
-            UIColor.brown,
-            UIColor.gray,
-            UIColor.blue,
-            UIColor.cyan,
-            UIColor.orange
+        let colors: [UIColor] = [
+            .red,
+            .green,
+            .brown,
+            .gray,
+            .blue,
+            .cyan,
+            .orange,
+            .darkGray,
+            .magenta,
+            .purple,
+            .yellow,
+            .white
         ]
         return colors[Int(arc4random_uniform(UInt32(colors.count)))]
     }
@@ -31,6 +36,15 @@ class ViewController: BaseViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let smallView1 = UIView(frame: CGRect(x: 50, y: 100, width: self.view.frame.width - 100, height: 100))
+        smallView1.backgroundColor = color
+        self.view.addSubview(smallView1)
+        
+        let smallView2 = UIView(frame: CGRect(x: 50, y: 300, width: self.view.frame.width - 100, height: 100))
+        smallView2.backgroundColor = color
+        self.view.addSubview(smallView2)
+        
         self.navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(add))
     }
     
